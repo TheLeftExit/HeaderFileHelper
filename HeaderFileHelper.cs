6884ba86b00fa37a3d2332319c0388b424e1b93a
TheLeftExit <TheLeftExit@DESKTOP-MBKKDI4>
2024-03-03 00:53:15 +0400

    Add project files.

---
+++ HeaderFileHelper.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+public static partial class HeaderFileHelper
+{
+    [GeneratedRegex(@"\ *//.*?(?=\r\n)", RegexOptions.Multiline)]
+    private static partial Regex SingleLineComments();
+    [GeneratedRegex(@"/\*.*?\*/", RegexOptions.Singleline | RegexOptions.Multiline)]
+    private static partial Regex MultiLineComments();
+    [GeneratedRegex(@"(?<=^)#.*?[^\\](?=\r\n)", RegexOptions.Singleline | RegexOptions.Multiline)]
+    private static partial Regex Directives();
+    [GeneratedRegex(@"[\\\s]+", RegexOptions.Multiline)]
+    private static partial Regex WhitespaceCharacters();
+
+    public static string WithoutComments(this string text)
+    {
+        text = SingleLineComments().Replace(text, "");
+        text = MultiLineComments().Replace(text, "");
+        return text;
+    }
+
+    public static IEnumerable<string> GetDirectives(this string text)
+    {
+        return Directives().Matches(text).Select(x => x.Value);
+    }
+
+    public static string RemoveDirectives(this string text)
+    {
+        return Directives().Replace(text, "");
+    }
+
+    public static IEnumerable<string> GetInstructions(this string text)
+    {
+        var instructionText = text.RemoveDirectives();
+        var semicolonIndexes = new List<int>();
+        var depth = 0;
+        var maxDepth = 0;
+        for (int i = 0; i < instructionText.Length; i++)
+        {
+            if (instructionText[i] == '{')
+            {
+                if (++depth == maxDepth) semicolonIndexes.Add(i);
+            }
+            else if (instructionText[i] == '}')
+            {
+                if (--depth == maxDepth - 1) semicolonIndexes.Add(i);
+            }
+            else if (instructionText[i] == ';' && depth <= maxDepth)
+            {
+                semicolonIndexes.Add(i);
+            }
+        }
+        return Enumerable
+            .Range(0, Math.Max(0, semicolonIndexes.Count - 2))
+            .Select(x => instructionText[(semicolonIndexes[x] + 1)..(semicolonIndexes[x + 1] + 1)]);
+    }
+
+    public static IEnumerable<string> CleanUp(this IEnumerable<string> lines)
+    {
+        return lines
+            .Select(x => WhitespaceCharacters().Replace(x, " ").Trim())
+            .Where(x => !string.IsNullOrEmpty(x));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+string[] headerFilePaths = [
+    @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared\windowsx.h",
+    @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um\WinUser.h",
+    @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um\CommCtrl.h"
+];*/
+
+var rootDir = @"C:\Program Files (x86)\Windows Kits\10\Include";
+var headerFilePaths = Directory.EnumerateFiles(rootDir, "*.h", SearchOption.AllDirectories)
+    .Where(x => !x.Contains("winrt"));
+
+var allDirectives = new List<string>();
+var allInstructions = new List<string>();
+
+foreach(var filePath in headerFilePaths)
+{
+    var file = File.ReadAllText(filePath);
+
+    var fileNoComments = file.WithoutComments();
+
+    var directives = fileNoComments
+        .GetDirectives()
+        .CleanUp();
+    allDirectives.AddRange(directives);
+
+    var instructions = fileNoComments
+        .GetInstructions()
+        .CleanUp();
+    allInstructions.AddRange(instructions);
+}
+
+;
