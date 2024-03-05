9a71734712cec7e99de8a3de5b46fd370bcfa005
TheLeftExit <theleftexit@theleftexit.net>
2024-03-05 23:20:33 +0400

    12321

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿/*
-string[] headerFilePaths = [
+﻿string[] headerFilePaths = [
     @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared\windowsx.h",
     @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um\WinUser.h",
     @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um\CommCtrl.h"
-];*/
+];
 
-var rootDir = @"C:\Program Files (x86)\Windows Kits\10\Include";
-var headerFilePaths = Directory.EnumerateFiles(rootDir, "*.h", SearchOption.AllDirectories)
-    .Where(x => !x.Contains("winrt"));
+//var rootDir = @"C:\Program Files (x86)\Windows Kits\10\Include";
+//var headerFilePaths = Directory.EnumerateFiles(rootDir, "*.h", SearchOption.AllDirectories)
+//    .Where(x => !x.Contains("winrt"));
 
 var allDirectives = new List<string>();
 var allInstructions = new List<string>();
@@ -28,5 +27,21 @@ foreach(var filePath in headerFilePaths)
         .CleanUp();
     allInstructions.AddRange(instructions);
 }
+
+var constantDefinitions = allDirectives
+    .Where(x =>
+    {
+        Span<Range> ranges = stackalloc Range[3];
+        var span = x.AsSpan().Slice(1);
+        if (span.Split(ranges, ' ') < 3) return false;
+
+        var directive = span[ranges[0]];
+        var symbol = span[ranges[1]];
+        var value = span[ranges[2]];
+
+        if (directive is not "define") return false;
+        foreach (char c in symbol) if (char.IsLower(c) || c is '(') return false;
+        return true;
+    }).ToList();
 
 ;
